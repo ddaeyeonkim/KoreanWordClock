e9fcf5ad3079fa56bf7d4eb6e86e879c2e287623
ddaeyeonkim <improve777@gmail.com>
2023-04-14 08:51:14 +0900

    chore: gradle kotlin dsl 적용

---
+++ app/build.gradle.kts
@@ -0,0 +1,44 @@
+@Suppress("DSL_SCOPE_VIOLATION")
+plugins {
+    id("com.pekka.koreanwordclock.android.application.compose")
+    alias(libs.plugins.ktlint)
+}
+android {
+    namespace = "com.pekka.koreanwordclock"
+
+    defaultConfig {
+        applicationId = "com.pekka.koreanwordclock"
+        targetSdk = libs.versions.targetSdk.get().toInt()
+        versionCode = 1
+        versionName = "1.0.0"
+
+        vectorDrawables {
+            useSupportLibrary = true
+        }
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+        }
+    }
+    packagingOptions {
+        resources {
+            excludes += "/META-INF/{AL2.0,LGPL2.1}"
+        }
+    }
+}
+
+ktlint {
+    android.set(true)
+    outputColorName.set("RED")
+}
+
+dependencies {
+    implementation(libs.bundles.android)
+    debugImplementation(libs.bundles.compose.debug)
+
+    testImplementation(libs.junit4)
+    androidTestImplementation(libs.bundles.test.android)
+}
