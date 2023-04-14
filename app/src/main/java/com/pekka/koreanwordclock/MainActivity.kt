8ac896b13d3672d93b9b781bc4b15a5347ef8f65
ddaeyeonkim <improve777@gmail.com>
2023-04-14 12:53:25 +0900

    feat: MainActivity에 한글시계 기능 구현

--- app/src/main/java/com/pekka/koreanwordclock/MainActivity.kt
+++ app/src/main/java/com/pekka/koreanwordclock/MainActivity.kt
@@ -2,17 +2,22 @@ package com.pekka.koreanwordclock
 
 import android.os.Bundle
 import androidx.activity.compose.setContent
+import androidx.activity.viewModels
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
 import com.pekka.common.activity.BaseActivity
-import com.pekka.koreanwordclock.ui.composables.Greeting
+import com.pekka.koreanwordclock.ui.home.HomeScreen
+import com.pekka.koreanwordclock.ui.home.HomeViewModel
 import com.pekka.koreanwordclock.ui.theme.KoreanWordClockTheme
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : BaseActivity() {
+
+    private val viewModel : HomeViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
@@ -22,9 +27,19 @@ class MainActivity : BaseActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    HomeScreen(viewModel)
                 }
             }
         }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        viewModel.start()
+    }
+
+    override fun onStop() {
+        viewModel.stop()
+        super.onStop()
     }
 }
